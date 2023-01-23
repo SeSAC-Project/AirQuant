83b20d2b40d69ca3e2be2b1d98d964e1c37b77bd
eunye <dms8945@gmail.com>
2023-01-23 13:45:53 +0900

    Docs: TopButton 버튼 클릭 시 깜빡임 수정

--- src/components/module/TopButton/TopButton.jsx
+++ src/components/module/TopButton/TopButton.jsx
@@ -1,67 +1,39 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { IoIosArrowUp } from 'react-icons/io';
 
 function TopButton() {
-  const [ScrollY, setScrollY] = useState(0);
   const [BtnStatus, setBtnStatus] = useState(false); // 버튼 상태
-  const topBtn = useRef();
-  console.log(topBtn.current);
 
-  const handleFollow = () => {
-    setScrollY(window.scrollY || document.documentElement.scrollTop);
-    if (ScrollY > 1000) {
-      // 1000 이상이면 버튼이 보이게
-      setBtnStatus(true);
-    } else {
-      // 1000 이하면 버튼이 사라지게
-      setBtnStatus(false);
-    }
-  };
-
-  const handleTop = () => {
-    // 클릭하면 스크롤이 위로 올라가는 함수
+  const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
-    setScrollY(0); // ScrollY 의 값을 초기화
-    setBtnStatus(false); // BtnStatus의 값을 false로 바꿈 => 버튼 숨김
   };
 
   useEffect(() => {
-    const watch = () => {
-      window.addEventListener('scroll', handleFollow);
+    const showClickbtn = () => {
+      if (window.scrollY > 1000) {
+        setBtnStatus(true);
+      } else {
+        setBtnStatus(false);
+      }
     };
-    watch();
+    window.addEventListener('scroll', showClickbtn);
     return () => {
-      window.removeEventListener('scroll', handleFollow);
+      window.removeEventListener('scroll', showClickbtn);
     };
-  });
-
+  }, []);
   return (
-    // <div>
-    //   <button
-    //     className={BtnStatus ? 'topBtn active' : 'topBtn'} // 버튼 노출 여부
-    //     onClick={handleTop} // 버튼 클릭시 함수 호출
-    //     ref={topBtn}
-    //   >
-    //     <IoIosArrowUp />
-    //   </button>
-    // </div>
-    <>
-      {BtnStatus && (
-        <div>
-          <button
-            className={BtnStatus ? 'topBtn active' : 'topBtn'} // 버튼 노출 여부
-            type="button"
-            onClick={handleTop} // 버튼 클릭시 함수 호출
-            ref={topBtn}
-          >
-            <IoIosArrowUp />
-          </button>
-        </div>
-      )}
-    </>
+    <div>
+      <button
+        className={BtnStatus ? 'topBtn' : 'topBtn-active'}
+        onClick={scrollToTop}
+        type="button"
+      >
+        <IoIosArrowUp />
+      </button>
+    </div>
   );
 }
 
